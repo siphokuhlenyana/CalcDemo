1e7185094945ae6f008a009076ac139354780eec
Siphokuhle <siphokuhlenyana@gmail.com>
2024-05-17 16:32:42 +0200

    added

---
+++ script/calc.js
@@ -0,0 +1,217 @@
+let buttons =document.getElementsByTagName('button')
+// for(let button of buttons ){
+//     button.addEventListener('click',()=>{
+
+    
+//     alert(button.value)
+    
+// })
+
+let input= document.querySelector('[data-input]')
+let button1=document.querySelector('[value="AC"]')
+let button2=document.querySelector('[value="%"]')
+let button3=document.querySelector('[value="."]')
+let button4=document.querySelector('[(value="9"]')
+let button5=documen.querySelector('[value="8"]')
+let button6=document.querySelector('[value="7"]')
+let button7=document.querySelector('[value="6"]')
+let button8=document.querySelector('[value="5"]')
+let button9=document.querySelector('[value="4"]')
+let button10=document.querySelector('[value="3"]')
+let button11=document.querySelector('[value="2")]')
+let button12=document.querySelector('[value="1"]')
+let button13=document.querySelector('[value="0"]')
+let button14=document.querySelector('[value="*"]')
+let button15=document.querySelector('[value="/"]')
+let button16=document.querySelector('[value="-"]')
+let button17=document.querySelector('[value="+"]')
+let button18=document.querySelector('[value="="]')
+
+
+button1.addEventListener('click',()=>{
+    
+button1.addEventListener('click',()=>{
+    input.value = button5.value
+})
+
+})
+
+
+button2.addEventListener('click',()=>{
+    input.value = button2.value
+})
+
+
+
+button3.addEventListener('click',()=>{
+    input.value = button3.value
+})
+
+
+button4.addEventListener('click',()=>{
+    input.value += button4.value
+})
+
+
+button5.addEventListener('click',()=>{
+    input.value += button5.value
+})
+
+
+button6.addEventListener('click',()=>{
+    input.value += button6.value
+})
+
+
+button7.addEventListener('click',()=>{
+    input.value += button7.value
+})
+
+
+button8.addEventListener('click',()=>{
+    input.value += button8.value
+})
+
+
+button9.addEventListener('click',()=>{
+    input.value += button9.value
+})
+
+
+button10.addEventListener('click',()=>{
+    input.value += button10.value
+})
+
+
+button11.addEventListener('click',()=>{
+    input.value += button11.value
+})
+
+
+button12.addEventListener('click',()=>{
+    input.value += button12.value
+})
+
+
+button13.addEventListener('click',()=>{
+    input.value += button13.value
+})
+
+
+button14.addEventListener('click',()=>{
+    input.value = button14.value
+})
+
+
+button15.addEventListener('click',()=>{
+    input.value = button15.value
+})
+
+button16.addEventListener('click',()=>{
+    input.value = button16.value
+})
+
+button17.addEventListener('click',()=>{
+    input.value = button17.value
+})
+
+button18.addEventListener('click',()=>{
+    input.value = button18.value
+})
+
+
+function modulus(){
+    input.value %2
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// button1.addEventListener("click",Clear)
+// button2.addEventListener("click",Modulus)
+// button3.addEventListener("click",Dot)
+// button4.addEventListener("click",clickNine)
+// button5.addEventListener("click",clickEight)
+// button6.addEventListener("click",clickSeven)
+// button7.addEventListener("click",ClickSix)
+// button8.addEventListener("click",ClickFive)
+// button9.addEventListener("click",ClickFour)
+// button10.addEventListener("click",ClickThree)
+// button11.addEventListener("click",ClickTwo)
+// button12.addEventListener("click",ClickOne)
+// button13.addEventListener("click",ClickZero)
+// button14.addEventListener("click",function)
+// button15.addEventListener("click",function)
+// button16.addEventListener("click",function)
+// button17.addEventListener("click",function)
+// button18.addEventListener("click",function)
+
+
+// function Clear(){
+//   input.value =button1.value;
+// }
+// function Modulus(){
+//     input.value =button2.value;
+//   }
+//   function Dot(){
+//     input.value =button3.value;
+//   }
+//   function clickNine(){
+//     input.value +=button4.value;
+//   }
+  
+//   function clickEight(){
+//     input.value +=button5.value;
+//   }
+  
+//   function clickSeven(){
+//     input.value +=button6.value;
+//   }
+  
+//   function ClickSix(){
+//     input.value +=button7.value;
+//   }
+  
+//   function ClickFive(){
+//     input.value +=button8.value;
+//   }
+  
+//   function ClickFour(){
+//     input.value +=button9.value;
+//   }
+  
+//   function ClickThree(){
+//     input.value +=button10.value;
+//   }
+  
+//   function ClickTwo(){
+//     input.value +=button11.value;
+//   }
+  
+//   function ClickOne(){
+//     input.value +=button12.value;
+//   }
+//   function ClickZero(){
+//     input.value +=button13.value;
+//   }
+//   function multiply(){
+//       input.value buttons.value
+//   }
+//    function divide(){
+//     input.value
+//    }
+  
+
+
+
+
+
